523cee50e472d0b00892e77cbd2f4d0a03923586
PamelaYanou <165833009+PamelaYanou@users.noreply.github.com>
2024-04-19 13:45:02 +0200

    Did the search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
-let searchBar = document.querySelector("#search-form");
-console.log(searchBar);
-searchBar.addEventListener("submit", showCity);
+function updateWeatherInfo(response) {
+  console.log(response.data.temperature.current);
+  let temperatureElement = document.querySelector("#meteo-app-figure");
+  let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
+  let h2 = document.querySelector("h2");
+  h2.innerHTML = response.data.city;
+}
+function callCity(city) {
+  let apiKey = `6d0cfo306t4a80c23b5feedc0cb2260c`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeatherInfo);
+}
 function showCity(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#search-input");
-  console.log(cityInput.value);
-  let h2 = document.querySelector("#meteo-city");
-  h2.innerHTML = `${cityInput.value}`;
+  callCity(cityInput.value);
 }
+let searchBar = document.querySelector("#search-form");
+console.log(searchBar);
+searchBar.addEventListener("submit", showCity);

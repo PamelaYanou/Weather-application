e4ef52961568a5342fde95dbc72c5422e9e0d41f
PamelaYanou <165833009+PamelaYanou@users.noreply.github.com>
2024-04-19 12:22:39 +0200

    Updated some class names

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
-function searchLogin() {
-  let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#meteo-city");
-  cityElement.innerHTML = searchInput.value;
-}
-
-let searchForms = document.querySelector("#search-form");
-searchForms.addEventListener("submit", searchLogin);
+let searchBar = document.querySelector(`#search-form`);

b9d0075089c679241ad55d7d5e0de18d99eb73ae
PamelaYanou <165833009+PamelaYanou@users.noreply.github.com>
2024-04-19 20:39:25 +0200

    Update more data

--- src/index.js
+++ src/index.js
@@ -6,7 +6,38 @@ function updateWeatherInfo(response) {
   h2.innerHTML = response.data.city;
   let descriptionElement = document.querySelector("#description");
   descriptionElement.innerHTML = response.data.condition.description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
 }
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
+}
+
 function callCity(city) {
   let apiKey = `6d0cfo306t4a80c23b5feedc0cb2260c`;
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
@@ -20,3 +51,8 @@ function showCity(event) {
 let searchBar = document.querySelector("#search-form");
 console.log(searchBar);
 searchBar.addEventListener("submit", showCity);
+callCity("Berlin");
+let currentDateELement = document.querySelector("#current-date");
+let currentDate = new Date();
+
+currentDateELement.innerHTML = formatDate(currentDate);

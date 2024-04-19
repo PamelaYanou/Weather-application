f059ca1250d47fbef4bf904b3541d0feedd3b7bd
PamelaYanou <165833009+PamelaYanou@users.noreply.github.com>
2024-04-19 12:52:11 +0200

    debug index.js file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-let searchBar = document.querySelector(`#search-form`);
+let searchBar = document.querySelector("#search-form");
+console.log(searchBar);
+searchBar.addEventListener("submit", showCity);
+function showCity(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#search-input");
+  console.log(cityInput.value);
+  let h2 = document.querySelector("#meteo-city");
+  h2.innerHTML = `${cityInput.value}`;
+}
